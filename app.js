e3532c184e3d613bb03c168bdb5ac159158f3af0
Gamalliel Sharon <you@example.com>
2020-09-19 13:49:07 +0700

    modify mobile version 1st

--- app.js
+++ app.js
@@ -82,3 +82,55 @@ modal.addEventListener("click", (e) => {
 //          }
 //   }); 
 // }
+
+
+
+
+const personName = document.querySelector('.testimonial-name');
+const personContent = document.querySelector('.testimonial-content');
+const arrows = document.querySelectorAll('.icon');
+const testiProfile = document.querySelector('.testi-profile');
+
+console.log(testiProfile.src)
+
+
+let testimonials = [
+  {
+    name: 'Susi Susanti', 
+    content: 'Saya jadi tidak bingung jika Mobil saya mogok di jalan karena ada AKM Towing.'
+  },
+  {
+    name: 'Hendra',
+    content: 'Pengiriman mobil cepat dan menjangkau semua area'
+  },
+  {
+    name: 'Susi',
+    content: 'Saya jadi tidak bingung jika Mobil saya mogok di jalan karena ada AKM Towing.'
+  }
+]
+
+
+console.log(arrows)
+
+for(const arrow of arrows) {
+  arrow.addEventListener('click', (e) => {
+    console.log(e.target.id);
+    // alert('oke');
+    // console.log(e.target)
+    let index = e.target.id;
+    if(index == 2){
+      renderContent(index) + 1;
+    }
+    else if(index == 1){
+      renderContent(index) - 1;
+    }
+    
+  })
+}
+
+renderContent(1);
+
+function renderContent(index){
+  personContent.innerText = `${testimonials[index].content}`;
+  personName.innerText = `${testimonials[index].name}`;
+}
